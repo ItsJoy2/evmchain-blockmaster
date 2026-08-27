9bef68a8167fb312514c8d7da2b5e4d2a8bdefae
AJ <edulife1999@gmail.com>
2026-08-27 12:59:46 +0600

    fixed wallet added

--- routes/console.php
+++ routes/console.php
@@ -3,9 +3,24 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
+use App\Jobs\ProcessDeposit;
+use App\Models\MerchantUserWallet;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
 Schedule::command('merchant:subscription')->everyFifteenMinutes();
+
+Schedule::call(function () {
+    MerchantUserWallet::where('is_active', true)
+        ->select('id')
+        ->chunkById(100, function ($wallets) {
+
+            foreach ($wallets as $wallet) {
+                ProcessDeposit::dispatch($wallet->id);
+            }
+
+        });
+
+})->everyMinute();
